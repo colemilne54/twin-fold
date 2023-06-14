312bf6de717a7ba36088ebcd921c126ace49d451
colemilne54 <colemilne54@gmail.com>
2023-06-14 12:20:01 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'twin.macro'
+
+function App() {
+
+  return (
+    <>
+      <h1 tw="text-sm" className="my-class">Vite + React</h1>
+    </>
+  )
+}
+
+export default App
